6007348c63bfebc325bfe0eb52b929e592bc2f49
mark johnson <markjohnson@Marks-MacBook-Pro.local>
2012-05-10 17:40:52 -0400

    added first function

--- johnson_mark_assignment2/johnson_mark_assignment.js
+++ johnson_mark_assignment2/johnson_mark_assignment.js
@@ -1,3 +1,39 @@
 // Mark Johnson
 // Project 2
 // SDI 1205
+
+var freeways = ["highway 55", "highway 42"],
+ 	bridgeOne = "schuykillExpressway", 
+	bridgeTwo = "benFranklinBridge",
+	tripOneMiles = 62,
+	tripTwoMiles = 67
+;
+
+// time of day funtion with a return and no arguments
+var timeOfDay () {
+	var certainTime = 4;
+	var currentTime = newDate()
+
+	if (timeOfDay >= certainTime) {
+		console.log("there will be too much traffic on both bridges. I'll take the " + bridgeOne + " to her house")
+	} // end if statement
+
+	return currentTime
+} 
+timeOfDay(currentTime) // time of day function being called
+
+var schuykillExpressway () {
+
+}
+
+var benFranklinBridge () {
+
+}
+
+
+// end time of day function
+
+
+
+var 
+var totalMiles []
