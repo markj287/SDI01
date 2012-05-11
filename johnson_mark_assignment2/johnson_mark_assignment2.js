53c5dda9eb4859d24850ffd649ec8927176b96d6
mark johnson <markjohnson@Marks-MacBook-Pro.local>
2012-05-10 21:07:35 -0400

    changes

--- johnson_mark_assignment2/johnson_mark_assignment2.js
+++ johnson_mark_assignment2/johnson_mark_assignment2.js
@@ -3,8 +3,33 @@
 // SDI 1205
 
 
+var charName = "Mark",
+	relatives = ["Mike","Dan","Dre","Dwayne"],
+	numOfCousins = 2,
+	numOfFriends = 6,
+	playBasketball = true,
+	impressGirls = true
+;
 
+// function with no arguments or no returns  
+var goSwimming = function () {
+	var certainTime = 6
+	var currentTime = new Date()
 
+	if (currentTime >= certainTime) {
+		console.log(charName + " It's a little too late to go swimming.")
+	} // end if statement
+}
+goSwimming()
+
+var goPlaying = function () {
+	var cousinsPresent = true 
+	var friendsPresent = true
+
+	if (cousinsPresent = true) {
+		console.log(charName + " says, we can go on the swings for about 30 minutes.")
+	}
+}
 
 
 
