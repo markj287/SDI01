fc4fb5a9d3cce825ddc974f413edc1654a258ee4
mark johnson <markjohnson@Marks-MacBook-Pro.local>
2012-05-17 09:21:12 -0400

    json data

---
+++ johnson_mark_assignment3/json.js
@@ -0,0 +1,40 @@
+
+var bradleyFamily = {
+	"members": [
+		{
+			"name": "mommacarla",
+			"age": 34,
+			"occupation": "nurse"
+		},
+		{
+			'name': 'daddyjohnathon',
+			'age': 37,
+			'occupation': 'truckdriver'
+		},
+		{
+			'name': 'gmomsuzzy',
+			'age': 57,
+			'occupation': 'retireddoctor'
+		},
+		{
+			'name': 'gpopbob',
+			'age': 62,
+			'occupation': 'retiredsurgeon'
+		},
+		{
+			'name': 'daughtersammy',
+			'age': 10,
+			'occupation': 'student'
+		},
+		{
+			'name': 'sondavid',
+			'age': 8,
+			'occupation': 'student'
+		},
+		{
+			'name': 'babyjasmin',
+			'age': '5 months',
+			'occupation': 'none'
+		}
+	]
+};
