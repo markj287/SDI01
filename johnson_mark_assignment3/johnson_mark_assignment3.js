b62c1910b1fab3405e64a577ba54e9ef315cefa0
mark johnson <markjohnson@Marks-MacBook-Pro.local>
2012-05-17 09:22:32 -0400

    json function

--- johnson_mark_assignment3/johnson_mark_assignment3.js
+++ johnson_mark_assignment3/johnson_mark_assignment3.js
@@ -1,3 +1,10 @@
 // Mark Johnson
 // Project 3
 
+// funtion for json data
+var handleData = function (bradleyFamily) {
+	for (i = 0; i < bradleyFamily.members.length; i++){
+		var member = bradleyFamily.members[i];
+		console.log('Family member name: ' + member.name + ', Age: ' + member.age + ', Occupation: ' + member.occupation);
+	};   // console log info about family 
+};
