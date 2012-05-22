6a5b12a78720c03dbf34159e94642e4f5fa2c7a9
mark johnson <markjohnson@Marks-MacBook-Pro.local>
2012-05-22 14:23:51 -0400

    starting over

--- johnson_mark_assignment4/johnson_mark_assignment4.js
+++ johnson_mark_assignment4/johnson_mark_assignment4.js
@@ -3,38 +3,3 @@
 
 
 // My javascript library 
-myLibray = {
- 	// method to check if its a phone number format 
- 	checkNumber: function (n) {
- 		var numPatt = /(^-*\d+$)|(^-*\d+\.\d+$)/;  // regular expression to check for a phone number 
-
- 		return numPatt.test(n)  // this is a regular expression test method that searches the string for a specified value
- 	},  // end checkNumber method 
-
- 	// method to check if its a email format
- 	checkEmail: function (e) {
- 		var emailPatt = /^[a-zA-Z0-9][a-zA-Z0-9-_\s]+@[a-zA-Z0-9-\s].+\.[a-zA-Z]{2,5}$/; // regular expression to check email format 
-
- 		return emailPatt.test(e) // this is a regular expression test method that searches the string for a specified value
- 	}, // end checkEmail method
-
- 	// method to if its an url
- 	checkUrl: function (u) {
- 		var urlPatt = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/ // regular expression to check url format 
-
-		return urlPatt.test(u) // this is a regular expression test method that searches the string for a specified value
- 	}, // end checkUrl method 
-
- 	// method to get the smallest value in an array
- 	getValue: function () {
- 	
-
- 	}, // ends getValue method 
-
-} // end my library method 
-
-// Condition to check if its a phone number
-
-
-
-console.log(myLibray.checkNumber)
