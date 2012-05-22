17a9b9d2ae62428369754e863afe1c1803ce9136
mark johnson <markjohnson@Marks-MacBook-Pro.local>
2012-05-22 18:30:34 -0400

    added another function

--- johnson_mark_assignment4/johnson_mark_assignment4.js
+++ johnson_mark_assignment4/johnson_mark_assignment4.js
@@ -14,8 +14,14 @@ var myLibrary = function () {
 		var emailPatt = /^([0-9a-zA-Z]+([_.-]?[0-9a-zA-Z]+)*@[0-9a-zA-Z]+[0-9,a-z,A-Z,.,-]*(.){1}[a-zA-Z]{2,4})+$/; // regular exp check the pattern of a email address
 	}
 
-return {
+	// Function to check url match 
+	var checkUrl = function () {
+		var urlPatt = new Regexp ("^(http[s]?://|ftp://)?(www\.)?[a-zA-Z0-9-\.]+\.(com|org|net|mil|edu|ca|co.uk|com.au|gov)$'"); // regular exp check the pattern of a url. I used the reg exp built in contructor method 
+	}
+
+	return {
 		"checkNumber": checkNumber,
-		"checkEmail": checkEmail
+		"checkEmail": checkEmail,
+		"checkUrl": checkUrl
 	}
 }
