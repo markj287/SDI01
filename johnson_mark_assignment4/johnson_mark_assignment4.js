26502e5011ec5fb56d45b036a9497eafeb7b5361
mark johnson <markjohnson@Marks-MacBook-Pro.local>
2012-05-22 18:51:00 -0400

    added a few more returns

--- johnson_mark_assignment4/johnson_mark_assignment4.js
+++ johnson_mark_assignment4/johnson_mark_assignment4.js
@@ -5,24 +5,27 @@
 // My revelaing Module Pattern  
 var myLibrary = function () {
 	// Function to check phone number match 
-	var checkNumber = function () {
+	var checkNumber = function (n) {
 		var numPatt = /^[2-9]\d{2}-\d{3}-\d{4}$/; // regular exp check the pattern of a phone number
 	}
 
 	// Function to check email match 
-	var checkEmail = function () {
+	var checkEmail = function (e) {
 		var emailPatt = /^([0-9a-zA-Z]+([_.-]?[0-9a-zA-Z]+)*@[0-9a-zA-Z]+[0-9,a-z,A-Z,.,-]*(.){1}[a-zA-Z]{2,4})+$/; // regular exp check the pattern of a email address
 	}
 
 	// Function to check url match 
-	var checkUrl = function () {
+	var checkUrl = function (u) {
 		var urlPatt = new Regexp ("^(http[s]?://|ftp://)?(www\.)?[a-zA-Z0-9-\.]+\.(com|org|net|mil|edu|ca|co.uk|com.au|gov)$"); // regular exp check the pattern of a url. I used the reg exp built in contructor method 
 	}
 
 	return {
-		"checkNumber": checkNumber,
-		"checkEmail": checkEmail,
-		"checkUrl": checkUrl
+		"number": checkNumber,
+		"numpatt": numpatt.test(n),
+		"email": checkEmail,
+		"emailPatt": emailPatt.test(e),
+		"url": checkUrl,
+		"urlPatt": urlPatt.test(u)
 	}
 }
 
