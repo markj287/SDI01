69b287a7dd0b1d6eca18967e1ba6c287c479c350
mark johnson <markjohnson@Marks-MacBook-Pro.local>
2012-05-22 17:09:56 -0400

    changes

--- johnson_mark_assignment4/johnson_mark_assignment4.js
+++ johnson_mark_assignment4/johnson_mark_assignment4.js
@@ -1,17 +1,21 @@
 // Mark Johnson
 // SDI 1205 Project 4 
+// My javascript library
 
-
-// My javascript library 
+// My revelaing Module Pattern  
 var myLibrary = function () {
-	
+	// Function to check phone number match 
+	var isNumber = function (n) {
+		var numberCheck = /^[2-9]\d{2}-\d{3}-\d{4}$/; // regular exp check the pattern of a phone number 
+	}
 
 
 
 
 
 	return {
-		"":,
-		"":,
+		"numberCheck": numberCheck,
 	}
 }
+
+console.log(myLibrary.isNumber)
