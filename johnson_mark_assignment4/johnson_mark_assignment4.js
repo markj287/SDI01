6d975ce17bd9bddbaeeaea0f96ed4a702cf504de
mark johnson <markjohnson@Marks-MacBook-Pro.local>
2012-05-22 18:12:41 -0400

    testing

--- johnson_mark_assignment4/johnson_mark_assignment4.js
+++ johnson_mark_assignment4/johnson_mark_assignment4.js
@@ -5,8 +5,8 @@
 // My revelaing Module Pattern  
 var myLibrary = function () {
 	// Function to check phone number match 
-	var isNumber = function (n) {
-		var numberCheck = /^[2-9]\d{2}-\d{3}-\d{4}$/; // regular exp check the pattern of a phone number 
+	var checkNumber = function () {
+		var numPatt = /^[2-9]\d{2}-\d{3}-\d{4}$/; // regular exp check the pattern of a phone number
 	}
 
 
@@ -14,8 +14,12 @@ var myLibrary = function () {
 
 
 	return {
-		"numberCheck": numberCheck,
+		"checkNumber": checkNumber,
 	}
 }
-
-console.log(myLibrary.isNumber)
+// condition to check if the phone number format is correct 
+if (checkNumber("250-555-4542")) {
+	console.log("That looks like a phone number to me")
+} else {
+	console.log("This is not a number!")
+}
