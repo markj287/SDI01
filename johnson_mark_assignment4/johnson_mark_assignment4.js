376db0dd86e6cba71af68bc60d98dcd38e7a85dc
mark johnson <markjohnson@Marks-MacBook-Pro.local>
2012-05-21 22:50:51 -0400

    minor changes

--- johnson_mark_assignment4/johnson_mark_assignment4.js
+++ johnson_mark_assignment4/johnson_mark_assignment4.js
@@ -9,13 +9,15 @@ myLibray = {
  		var numPatt = /(^-*\d+$)|(^-*\d+\.\d+$)/;  // regular expression to check for a phone number 
 
  		return numPatt.test(n)  // this is a regular expression test method that searches the string for a specified value
- 	}  // end checkNumber function  
+ 	}  // end checkNumber method 
 
+ 	// method to check if its a email format
  	checkEmail: function (e) {
  		var emailPatt = /^[a-zA-Z0-9][a-zA-Z0-9-_\s]+@[a-zA-Z0-9-\s].+\.[a-zA-Z]{2,5}$/; // regular expression to check email format 
 
  		return emailPatt.test(e) // this is a regular expression test method that searches the string for a specified value
- 	} // end 
+ 	} // end checkEmail method
+
 
 
 }
